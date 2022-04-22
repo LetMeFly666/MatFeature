b1cea07d9e99e6d113fc9a92a3926424e0544501
LetMeFly666 <814114971@qq.com>
2022-04-22 16:19:49 +0800

    还原main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2022-04-22 15:43:04
  * @LastEditors: LetMeFly
- * @LastEditTime: 2022-04-22 16:12:53
+ * @LastEditTime: 2022-04-22 16:19:32
  */
 #include <iostream>
 #include <Eigen/Dense>
@@ -12,14 +12,6 @@ using Eigen::VectorXd;
 using namespace std;
 
 int main() {
-
-    // 图标生成
-    for (int i = 0; i < 10000; i++)
-        cout << rand() % 2;
-    puts("");
-    system("pause");
-    exit(0);
-
     MatrixXd m = MatrixXd::Random(3, 3);
     m = (m + MatrixXd::Constant(3, 3, 1.2)) * 50;
     std::cout << "m =" << std::endl << m << std::endl;
