80f46d4e41d74e9d6ca9fabbf2f9399497731726
LetMeFly666 <814114971@qq.com>
2022-04-26 12:19:43 +0800

    1. Ignore了老师编写的PDF文件 2. 构建了MatFeature类

--- main.cpp
+++ main.cpp
@@ -2,8 +2,9 @@
  * @Author: LetMeFly
  * @Date: 2022-04-22 21:07:21
  * @LastEditors: LetMeFly
- * @LastEditTime: 2022-04-25 21:45:19
+ * @LastEditTime: 2022-04-26 12:19:36
  */
+#include <MatFeature/MatFeature>
 #include <MatFeature/Reader>
 #include <Eigen/Dense>
 #include <iostream>
@@ -14,7 +15,7 @@ using Eigen::MatrixXd;
 int main() {
     string sourceFile = "Datas/sample01.txt";
     MatrixXd mat = Read(sourceFile.c_str());
-    cout << mat << endl;
+    MatFeature matFeature = MatFeature(mat);
     system("pause");
     return 0;
 }
