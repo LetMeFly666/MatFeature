9aeafa78ff9bb6799d983ccd6252adb68ce6362f
LetMeFly666 <814114971@qq.com>
2022-04-26 14:46:16 +0800

    1. 修改了Test的debug函数，使其能够打印[] 2. 实现了总共求一个值到每列求一个值的转型 3. 峭度不同之外其他都通过了验证

--- main.cpp
+++ main.cpp
@@ -2,29 +2,35 @@
  * @Author: LetMeFly
  * @Date: 2022-04-22 21:07:21
  * @LastEditors: LetMeFly
- * @LastEditTime: 2022-04-26 14:04:43
+ * @LastEditTime: 2022-04-26 14:39:08
  */
 #include <MatFeature/MatFeature>
 #include <MatFeature/Reader>
+#include <MatFeature/Test>
 #include <Eigen/Dense>
 #include <iostream>
 #include <cstring>
+#include <vector>
 using namespace std;
 using Eigen::MatrixXd;
+
+#define show1feature(featureName, featureGettingMethod)\
+    cout << featureName << ": ";\
+    debug(featureGettingMethod)
 
 int main() {
     string sourceFile = "Datas/sample01.txt";
     MatrixXd mat = Read(sourceFile.c_str());
     MatFeature matFeature = MatFeature(mat);
 
-    cout << "Maxium: " << matFeature.maximum() << endl;
-    cout << "Minium: " << matFeature.minimum() << endl;
-    cout << "Mean: " << matFeature.mean() << endl;
-    cout << "Peak: " << matFeature.peak() << endl;
-    cout << "Ma: " << matFeature.ma() << endl;
-    cout << "Var: " << matFeature.var() << endl;
-    cout << "Std: " << matFeature.std() << endl;
-    cout << "Kurtosis: " << matFeature.kurtosis() << endl;
+    show1feature("Maximum", matFeature.maximum());
+    show1feature("Minimum", matFeature.minimum());
+    show1feature("Mean", matFeature.mean());
+    show1feature("Peak", matFeature.peak());
+    show1feature("Ma", matFeature.ma());
+    show1feature("Var", matFeature.var());
+    show1feature("Std", matFeature.std());
+    show1feature("Kurtosis", matFeature.kurtosis());
 
     return 0;
 }
