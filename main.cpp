c4fa5e2003c72db3769b59909b4f3070c727ccf4
LetMeFly666 <814114971@qq.com>
2022-04-26 14:05:42 +0800

    又实现了几个功能，忽然发现了错误之处：是要每一行求一个

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
  * @Author: LetMeFly
  * @Date: 2022-04-22 21:07:21
  * @LastEditors: LetMeFly
- * @LastEditTime: 2022-04-26 12:19:36
+ * @LastEditTime: 2022-04-26 14:04:43
  */
 #include <MatFeature/MatFeature>
 #include <MatFeature/Reader>
@@ -16,6 +16,15 @@ int main() {
     string sourceFile = "Datas/sample01.txt";
     MatrixXd mat = Read(sourceFile.c_str());
     MatFeature matFeature = MatFeature(mat);
-    system("pause");
+
+    cout << "Maxium: " << matFeature.maximum() << endl;
+    cout << "Minium: " << matFeature.minimum() << endl;
+    cout << "Mean: " << matFeature.mean() << endl;
+    cout << "Peak: " << matFeature.peak() << endl;
+    cout << "Ma: " << matFeature.ma() << endl;
+    cout << "Var: " << matFeature.var() << endl;
+    cout << "Std: " << matFeature.std() << endl;
+    cout << "Kurtosis: " << matFeature.kurtosis() << endl;
+
     return 0;
 }
